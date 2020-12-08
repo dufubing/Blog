6f47061897da5b499fd225bf8444084d3cd01d40
1017964287@qq.com <l>
2020-12-08 15:00:21 +0800

    first

---
+++ blog/urls.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('index/', Index.as_view(), name='index'),
+    path('about/', About.as_view(), name='about'),
+    path('archive/', Archive.as_view(), name='archive'),
+    path('link/', Link.as_view(), name='link'),
+    path('message', Message.as_view(), name='message'),
+    path('article/<int:pk>/', Articles.as_view(), name='article'),
+    path('get_comment/', GetComment, name='get_comment'),
+    path('detail/<int:pk>/', Detail.as_view(), name='detail'),
+    path('search/', Search.as_view(), name='search'),
+    path('tag/<int:id>/', Tagcloud.as_view(), name='tag'),
+
+]
